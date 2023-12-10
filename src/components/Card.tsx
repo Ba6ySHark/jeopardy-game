f36d5cf53d2ccb0fe0199ca51cbbebd60c320fdf
Stepan Susorov <steven.susorov@gmail.com>
2023-12-09 22:37:36 -0700

    bug fix

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -29,7 +29,6 @@ export default function Card(props: any): JSX.Element {
                 break;
             }
         }
-        setNextPlayer();
         setCorrect(answer);
         setAnswered(true);
     }
